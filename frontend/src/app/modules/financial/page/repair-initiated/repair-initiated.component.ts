d002d63602914bf7c30cbd6f8ed67b687bc1d483
FelanaRasata <77356909+FelanaRasata@users.noreply.github.com>
2023-01-29 22:22:46 +0300

    Update repair-initiated.component.ts

--- frontend/src/app/modules/financial/page/repair-initiated/repair-initiated.component.ts
+++ frontend/src/app/modules/financial/page/repair-initiated/repair-initiated.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core'
 import { RepairService } from '../../../shared/core/services/repair/repair.service'
 import { logMessages } from '@angular-devkit/build-angular/src/builders/browser-esbuild/esbuild'
+import { Router } from '@angular/router'
 
 
 @Component({
@@ -10,13 +11,14 @@ import { logMessages } from '@angular-devkit/build-angular/src/builders/browser-
 })
 export class RepairInitiatedComponent {
     constructor(
-        public repairService: RepairService
+        public repairService: RepairService,
+        public router: Router
     ) {
     }
 
     validateFinance(){
         this.repairService.financeValidate(this.repairService.repair.value._id).subscribe({
-            next:() => console.log("SUCCESS")
+            next:() => this.router.navigate(['/financial'])
         })
     }
 }
